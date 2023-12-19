54f33123146de29a25b55baa40670989657ab983
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2023-12-20 00:36:51 +0300

    Features/set up prisma (#10)
    - [x] Настроил Prisma
    - [x] Настроил config и .env файл
    - [x] Сделал базовые модели User и Token

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+  await app.listen(PORT);
+  console.log('Server start on port ' + PORT);
 }
 bootstrap();
