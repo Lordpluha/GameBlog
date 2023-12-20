f465d8e3ecfba9e44503fd5d1101f3f83bb94c40
Going <goinghiggvp@gmail.com>
2023-12-20 23:04:15 +0300

    UserModule and UserService base

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfigOptions } from './configs/env.config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(EnvConfigOptions), AuthModule],
+  imports: [ConfigModule.forRoot(EnvConfigOptions), AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
---
+++ nest-server/src/user/constants/error.constants.user.ts
@@ -0,0 +1,1 @@
+export const USER_WITH_EMAIL_ALREADY_EXISTS = 'Пользователь с таким email уже существует'
