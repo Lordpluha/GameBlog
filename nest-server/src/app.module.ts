42d9c1eca4fb0bf7809853e3095e65ffd47bc255
Going <goinghiggvp@gmail.com>
2023-12-24 16:30:50 +0300

    file service

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -3,10 +3,9 @@ import { ConfigModule } from '@nestjs/config'
 import { EnvConfigOptions } from './configs/env.config'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
+import { FileModule } from './file/file.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(EnvConfigOptions), AuthModule, UserModule],
-	controllers: [],
-	providers: []
+	imports: [ConfigModule.forRoot(EnvConfigOptions), AuthModule, UserModule, FileModule]
 })
 export class AppModule {}
