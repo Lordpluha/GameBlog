b6368ed8c58b6c66ada98a472afb16830ec150de
Vladyslav <109725756+Lordpluha@users.noreply.github.com>
2023-12-25 12:15:08 +0000

    Delete component testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
-import ArticleList from "./components/ui/Article/ArticleList/ArticleList"
 
 function App() {
 	return (
 		<>
-			<h1>GameBlog</h1>
+			
 			<BrowserRouter>
-				<ArticleList articleList={articleList} />
+				<h1>GameBlog</h1>
 			</BrowserRouter>
 		</>
 	)
