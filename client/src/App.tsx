1316b5a8f544d65e5bcc95550e2799fb4ebdf984
kiberchainik <ya.sacher@gmail.com>
2023-12-25 13:11:12 +0100

    modifed app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
 import ArticleList from "./components/ui/Article/ArticleList/ArticleList"
-import { articleList } from './components/ui/exampleServerData/ArticleList.data'
 
 function App() {
 	return (
