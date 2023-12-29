1c5ef3928a8bfdb94624a7c7d04805cc3d1f79cf
Vladislav <109725756+Lordpluha@users.noreply.github.com>
2023-12-29 22:01:04 +0100

    Add custome team theme

---
+++ client/.storybook/VladosTeam.theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+	base: 'dark',
+	brandTitle: 'GameBlog Storybook',
+	brandImage: '../src/stories/assets/team-logo.png',
+	brandTarget: '_self'
+})
