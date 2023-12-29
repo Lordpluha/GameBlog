4c269a27f0876bef5e92eb59e554f7d642d135da
Vladislav <109725756+Lordpluha@users.noreply.github.com>
2023-12-29 22:31:06 +0100

    Update VladosTeam.theme.ts
    - add link url

--- client/.storybook/VladosTeam.theme.ts
+++ client/.storybook/VladosTeam.theme.ts
@@ -4,5 +4,6 @@ export default create({
 	base: 'dark',
 	brandTitle: 'GameBlog Storybook',
 	brandImage: '../src/stories/assets/team-logo.png',
-	brandTarget: '_self'
+	brandTarget: '_self',
+	brandUrl: 'https://github.com/Lordpluha/GameBlog'
 })
