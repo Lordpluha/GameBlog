8c7ad282ad5efdfe77789ca35badae0499c1df1f
kiberchainik <ya.sacher@gmail.com>
2023-12-29 14:26:48 +0100

    update bg-color in layout component

--- client/src/components/ui/Layout.tsx
+++ client/src/components/ui/Layout.tsx
@@ -3,7 +3,7 @@ import Header from './header/Header'
 
 const Layout = () => {
   return (
-    <div className='mx-auto h-screen bg-white text-[#1c2c38] dark:text-white dark:bg-[#161b1d] flex flex-col'>
+    <div className='mx-auto h-screen bg-white text-[#1c2c38] dark:text-white dark:bg-[#1c2124] flex flex-col'>
         <Header />
         <div className="container mt-[90px]">
           <Outlet />
