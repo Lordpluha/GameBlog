461893e1d990e524c2bbc85652730cb305e6271d
Going <goinghiggvp@gmail.com>
2023-12-31 03:16:11 +0300

    add fucntion create article in seeder

--- nest-server/seeder/seed.ts
+++ nest-server/seeder/seed.ts
@@ -38,11 +38,45 @@ const createBlogs = async (quantity: number) => {
 	return blogs.length
 }
 
+const createArticles = async (quantity: number) => {
+	const articles = []
+	const previewUrl = (await findByExtension()) ?? faker.image.urlPicsumPhotos()
+	const anyTags = []
+	const quantityTags = Math.round(Math.random() * 6)
+	for (let i = 0; i < quantityTags; i++) {
+		anyTags[i] = faker.commerce.department()
+	}
+	for (let i = 0; i < quantity; i++) {
+		const title = faker.lorem.words({ max: 15, min: 2 })
+		const content = faker.lorem.words({ max: 50, min: 20 })
+		const sl = generateSlug(title + '-' + Math.ceil(Math.random() * 100))
+		const article = await prisma.article.create({
+			data: {
+				content,
+				title,
+				slug: sl,
+				preview: previewUrl,
+				anyTags: anyTags.join(),
+				author: {
+					connect: {
+						id: 1
+					}
+				}
+			}
+		})
+		articles.push(article)
+	}
+
+	return articles.length
+}
+
 const main = async () => {
 	try {
 		console.log('Start sedding...')
 		const count = await createBlogs(10)
 		console.log(`Created ${count}  blogs`)
+		const c = await createArticles(10)
+		console.log(`Created ${c}  articles`)
 		console.log('End sedding.')
 	} catch (e) {
 		console.error(e)
