8dcbd871bbb6a7ce412ecb619a3599f043d62a66
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2024-01-28 23:25:47 +0300

    File changes
    Header component, styles folder

--- layout/components/Header/js/main.js
+++ layout/components/Header/js/main.js
@@ -9,7 +9,7 @@
 // }
 
 
-
+// icon burger
 const burger = document.querySelector('.header__burger');
 
 const checkWidth = () => {
@@ -20,12 +20,45 @@ window.addEventListener('resize', checkWidth);
 checkWidth();
 
 
+// dropdown on click button.theme
+const click = document.getElementById("choose-theme");
+click.style.opacity = 0;
 function show_hide() {
-  var click = document.getElementById("choose-theme");
+  let click = document.getElementById("choose-theme");
   if (click.style.opacity === "0") {
-     click.style.opacity = "1";
+    click.style.opacity = "1";
   } else {
-     click.style.opacity = "0";
+    click.style.opacity = "0";
   }
 }
+// let element = document.getElementById("choose-theme");
+// element.classList.add('active');
 
+// let element = document.getElementById("choose-theme");
+// element.addEventListener("click", () => {
+//   element.classList.add("active");
+// });
+
+
+// login on click button.user
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let person = document.getElementById("choose-icon");
+//   let openWindow = document.getElementById("header__user");
+//   openWindow.addEventListener("click", () => {
+//     person.classList.toggle("active");
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let person = document.getElementById("choose-icon");
+  let openWindow = document.getElementById("header__user");
+  openWindow.addEventListener("click", () => {
+    person.classList.toggle("active");
+  });
+  document.addEventListener("click", e => {
+    if (!e.target.closest('#choose-icon') && !e.target.closest('#header__user')) {
+      person.classList.remove('active');
+    }
+  });
+});
