2cf01f09f37153ed8c567d7af7818972f7671ca6
kiberchainik <ya.sacher@gmail.com>
2024-01-30 13:07:52 +0100

    breadcrumbs

--- client/src/components/ui/breadcrumbs/Breadcrumbs.tsx
+++ client/src/components/ui/breadcrumbs/Breadcrumbs.tsx
@@ -1,14 +1,40 @@
-import { useMatches } from 'react-router-dom'
+import { useLocation, useMatches } from 'react-router-dom'
 import styles from './breadcrumbs.module.scss'
+import Crumb from './Crumb'
 /**
  * Reused component of breadcrambs
  */
+type TCrumbs = {
+  path: string
+  title: string
+}[]
+
 const Breadcrumbs = () => {
-  const matches = useMatches()
+  const location = useLocation()
+  const crumbs:TCrumbs = [{
+    path:'',
+    title:''
+  }]
+
+  const matches = location.pathname.split('/')
+
+  matches.map((crumb, idx) => {
+      crumbs[idx] = {
+        path: crumb === ''? '/' : crumbs[idx-1].path+'/'+crumb,
+        title: crumb === ''? 'Home' : crumb
+      }
+  })
+console.log(crumbs);
+
   return (
     <div className={styles.breadcrumbs}>
       <ol>
-        Главная / Новости
+        {crumbs.map((crumb, idx) => 
+          <div>
+            {idx > 0 && <div className=''> / </div>}
+            <Crumb path={crumb.path} title={crumb.title} />
+          </div>
+        )}
       </ol>
     </div>
   )
--- client/src/components/ui/breadcrumbs/Crumb.tsx
+++ client/src/components/ui/breadcrumbs/Crumb.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 
-const Crumb = (crumb:string) => {
+const Crumb = ({path, title}:{path:string, title:string}) => {
+  title = title.replace('_', ' ')
+  const titleUpp = title[0].toUpperCase() + title.slice(1)
+
   return (
-    <Link to={`/${crumb}`}>{crumb}</Link>
+    <Link to={'/'+path}>{titleUpp}</Link>
   )
 }
 
