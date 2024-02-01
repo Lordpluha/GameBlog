4b564aa2fbb1074327d9dd08c13674cae7424eee
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-02-01 15:05:39 +0100

    Fix storybook

--- client/.storybook/manager.ts
+++ client/.storybook/manager.ts
@@ -1,5 +1,6 @@
 import { addons } from '@storybook/manager-api'
-import customTheme from './VladosTeam.theme'
+
+import customTheme from './theme/VladosTeam.theme'
 
 addons.setConfig({
 	theme: customTheme
--- client/.storybook/theme/VladosTeam.theme.ts
+++ client/.storybook/theme/VladosTeam.theme.ts
@@ -3,7 +3,9 @@ import { create } from '@storybook/theming/create'
 export default create({
 	base: 'dark',
 	brandTitle: 'GameBlog Storybook',
-	brandImage: '../src/stories/assets/team-logo.png',
-	brandTarget: '_self',
+	brandImage:
+		'https://raw.githubusercontent.com/Lordpluha/GameBlog/79df7ada969d569c01a83a901c7d0b1bb9e3ac61/client/src/stories/assets/team-logo.png',
+
+	brandTarget: '_blank',
 	brandUrl: 'https://github.com/Lordpluha/GameBlog'
 })
