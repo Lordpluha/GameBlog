ea6bbc231ff0a08e46a10400d8b9c80950e45468
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-02-01 15:05:39 +0100

    Fix storybook

--- client/.storybook/theme/VladosTeam.theme.ts
+++ client/.storybook/theme/VladosTeam.theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create'
 export default create({
 	base: 'dark',
 	brandTitle: 'GameBlog Storybook',
-	brandImage: '../src/stories/assets/team-logo.png',
+	brandImage: './assets/team-logo.png',
 	brandTarget: '_self',
 	brandUrl: 'https://github.com/Lordpluha/GameBlog'
 })
