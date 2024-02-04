f4c9beb34822f68a2e40ee9c57cf6a0793346bf5
ANNA <89228060160q@gmail.com>
2024-02-04 16:17:23 +0300

    add features

--- client/src/components/ui/Layout.tsx
+++ client/src/components/ui/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
 import Header from './Header/Header'
+import { Footer } from './footer/Footer'
 
 const Layout = () => {
 	return (
@@ -9,6 +10,7 @@ const Layout = () => {
 			<div className='container mt-[90px]'>
 				<Outlet />
 			</div>
+			<Footer />
 		</div>
 	)
 }
