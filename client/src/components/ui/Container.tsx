de079bb33d7dbf87260c5f73659d6a66a1c58f33
ANNA <89228060160q@gmail.com>
2024-02-10 21:23:08 +0300

    delete container

--- client/src/components/ui/Container.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export type TContainerProps = PropsWithChildren
-
-/** Basic layout container component */
-export const Container = ({ children }: TContainerProps) => {
-	return <div className='max-w-screen-xl'>{children}</div>
-}
