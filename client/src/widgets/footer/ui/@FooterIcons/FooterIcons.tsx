6534cc17042c65d394828b386c36bda7bf2be2df
ANNA <89228060160q@gmail.com>
2024-02-10 22:39:01 +0300

    change footericons.module.scss

--- client/src/widgets/footer/ui/@FooterIcons/FooterIcons.tsx
+++ client/src/widgets/footer/ui/@FooterIcons/FooterIcons.tsx
@@ -8,12 +8,18 @@ const FooterIcons = () => {
 	return (
 		<ul className={styles.footer__icons}>
 			<li>
-				<Youtube />
-				<div className={styles.tooltip}>Группа StopGame на Youtube</div>
+				<Link className={styles.social_icon} to=''>
+					<Youtube />
+					<div className={styles.tooltip}>
+						Группа StopGame на Youtube
+					</div>
+				</Link>
 			</li>
 			<li>
-				<VK />
-				<div className={styles.tooltip}>Группа StopGame на VK</div>
+				<Link className={styles.social_icon} to=''>
+					<VK />
+					<div className={styles.tooltip}>Группа StopGame на VK</div>
+				</Link>
 			</li>
 			<li>
 				<div className={styles.dropdown}>
@@ -31,8 +37,12 @@ const FooterIcons = () => {
 				</div>
 			</li>
 			<li>
-				<Steam />
-				<div className={styles.tooltip}>Страница StopGame на Steam</div>
+				<Link className={styles.social_icon} to=''>
+					<Steam />
+					<div className={styles.tooltip}>
+						Страница StopGame на Steam
+					</div>
+				</Link>
 			</li>
 		</ul>
 	)
