03d891068d7323e30295205ae88f70b33fc2c844
Dasha <153850235+kowkaLYT@users.noreply.github.com>
2024-02-11 23:57:24 +0300

    Responsive burger menu

--- layout/components/Header/js/main.js
+++ layout/components/Header/js/main.js
@@ -9,6 +9,60 @@ const checkWidth = () => {
 window.addEventListener('resize', checkWidth);
 checkWidth();
 
+//accordion burger icon
+document.querySelectorAll('.header__burger-content').forEach(function (content) {
+  content.style.display = 'none';
+});
+
+document.querySelectorAll('.header__accordion').forEach(function (accordion) {
+  accordion.addEventListener('click', function (e) {
+    e.preventDefault();
+    let thisAccordion = this;
+    let nextContent = thisAccordion.nextElementSibling;
+    if (nextContent.classList.contains('show')) {
+      nextContent.classList.remove('show');
+      nextContent.style.display = 'none';
+      thisAccordion.parentElement.parentElement
+        .querySelectorAll('.header__accordion').forEach(function (acc) {
+          acc.classList.remove('open');
+        });
+    } else {
+      thisAccordion.parentElement.parentElement
+        .querySelectorAll('.header__burger-content').forEach(function (content) {
+          content.classList.remove('show');
+          content.style.display = 'none';
+        });
+      nextContent.classList.add('show');
+      nextContent.style.display = 'block';
+      thisAccordion.parentElement.parentElement
+        .querySelectorAll('.header__accordion').forEach(function (acc) {
+          acc.classList.remove('open');
+        });
+
+      thisAccordion.classList.add('open');
+    }
+  });
+});
+
+//switch theme
+const light = document.getElementById('header__change-light');
+light.addEventListener('change', () => {
+	document.body.classList.toggle('light');
+});
+const dark = document.getElementById('header__change-dark');
+dark.addEventListener('change', () => {
+	document.body.classList.remove('light');
+});
+const checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (event) => {
+      checkboxes.forEach((otherCheckbox) => {
+        if (otherCheckbox !== checkbox) {
+          otherCheckbox.checked = false;
+        }
+      });
+    });
+  });
 
 // dropdown on click button.theme
 document.addEventListener("DOMContentLoaded", function () {
@@ -48,12 +102,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // background transparent on click
 function on() {
-  document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.display = "flex";
 }
 
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
 window.onclick = function (event) {
   let overlay = document.getElementById("overlay");
   if (event.target === overlay) {
@@ -82,6 +137,27 @@ document.addEventListener("DOMContentLoaded", function () {
       person.classList.remove("active");
     }
   });
+});
+
+
+// login window
+document.addEventListener("DOMContentLoaded", () => {
+  const login = document.getElementById("login");
+  const register = document.getElementById("fill-in");
+  const go = document.getElementById("sign-up");
+  const back = document.getElementById("back");
+  if (login && register && go && back) {
+    login.style.display = "block";
+    register.style.display = "none";
+    go.addEventListener("click", () => {
+      login.style.display = "none";
+      register.style.display = "block";
+    });
+    back.addEventListener("click", () => {
+      login.style.display = "block";
+      register.style.display = "none";
+    });
+  }
 });
 
 
