66e57a988912a0aed76812bd76c1aff844c7dfa7
kiberchainik <ya.sacher@gmail.com>
2024-02-18 15:20:38 +0100

    api article

--- client/src/pages/News/ui/AboutNewPage.tsx
+++ client/src/pages/News/ui/AboutNewPage.tsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-
-//import { IPublication } from '@model/interfaces'
 
 import { SliderReadAlso } from '@widgets/Sliders'
 
 import { INewsNavigation } from '../model/@interfaces'
 import FullNewsComponent from './@FullNewsPage/FullNewsComponent'
-import { useGetNewsWithSlugQuery } from '@store/@api/NewsApi'
+import { useGetNewsBySlugQuery } from '@store/@api/NewsApi'
 
 const AboutNewPage = () => {
-	//const [NewData, setNewData] = useState<IPublication>()
 	const [otherNews, setOtherNews] = useState<INewsNavigation[]>([])
 	const {slug} = useParams<string>()
-	const {isError, isLoading, data} = useGetNewsWithSlugQuery(slug)
-
-	// useEffect(() => {
-	// 	setNewData(data)
-	// }, [])
+	const {data} = useGetNewsBySlugQuery(slug)
 
 	if (!data || !otherNews) return <p>Loading...</p>
 
